defae0487bc4ddd8a2cf831c24bde6f5a7a7cf83
HichamBENKADA <hicham20014@gmail.com>
2024-02-14 02:30:23 -0500

    search btn click handler updated

--- src/components/SearchBtn.jsx
+++ src/components/SearchBtn.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { SlMagnifier } from "react-icons/sl";
 
 const SearchBtn = ({setSearch}) => {
+  const searchRef = useRef(null)
   return (
     <div className='searchBtn'>
         <input
           onChange={(e) => setSearch(e.target.value)}
+          ref = {searchRef}
           type="search"
           placeholder="Search for Anime"/>
-          <button><SlMagnifier/></button>
+          
+          {/* both event listeners accomplish the same task. changing search state and evoking data fitching  */}
+          <button onClick={(e) => setSearch(searchRef.current.value)}><SlMagnifier/></button>
     </div>
   )
 }
